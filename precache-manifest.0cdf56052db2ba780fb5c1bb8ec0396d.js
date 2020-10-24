e6508e2784381ebbdaad9d07edface6dfd8c966f
ayanisation <ayanizationreaction@gmail.com>
2020-10-24 12:08:52 +0530

    Updates

---
+++ precache-manifest.0cdf56052db2ba780fb5c1bb8ec0396d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2b00e82e4e120afd438c3628b7dc69",
+    "url": "/Dev_tutorials/index.html"
+  },
+  {
+    "revision": "c63f2f6795771534789d",
+    "url": "/Dev_tutorials/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "4ba02425cffb583d4b58",
+    "url": "/Dev_tutorials/static/css/main.a464e301.chunk.css"
+  },
+  {
+    "revision": "c63f2f6795771534789d",
+    "url": "/Dev_tutorials/static/js/2.fa1bc4e9.chunk.js"
+  },
+  {
+    "revision": "7d0b5c1cc1715c8844e100015674cba1",
+    "url": "/Dev_tutorials/static/js/2.fa1bc4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba02425cffb583d4b58",
+    "url": "/Dev_tutorials/static/js/main.5aff5269.chunk.js"
+  },
+  {
+    "revision": "4923566b4d536daf936c",
+    "url": "/Dev_tutorials/static/js/runtime-main.d1db4d11.js"
+  },
+  {
+    "revision": "f6662578d8943b9a9d112003065c49f9",
+    "url": "/Dev_tutorials/static/media/businessladies.f6662578.svg"
+  },
+  {
+    "revision": "8c66e9c40ab68057c0698b31b443aa2a",
+    "url": "/Dev_tutorials/static/media/flutter.8c66e9c4.png"
+  },
+  {
+    "revision": "aefd1e38b4b45d3800b43b726a8d7630",
+    "url": "/Dev_tutorials/static/media/kotlin_android.aefd1e38.png"
+  },
+  {
+    "revision": "f34a0218dd2e38726c52ac763df55fa7",
+    "url": "/Dev_tutorials/static/media/nodejs.f34a0218.jpeg"
+  },
+  {
+    "revision": "e9ecdd4f1ee2f7ca4f877544dd35e1b5",
+    "url": "/Dev_tutorials/static/media/nosql.e9ecdd4f.jpg"
+  },
+  {
+    "revision": "384ba127aa138ecbe98831f06799d7da",
+    "url": "/Dev_tutorials/static/media/programmer.384ba127.svg"
+  },
+  {
+    "revision": "370c97b6d1baa3709149f819902df1f6",
+    "url": "/Dev_tutorials/static/media/react.370c97b6.png"
+  },
+  {
+    "revision": "19d2daac2522846084872ca2eab59fff",
+    "url": "/Dev_tutorials/static/media/sql.19d2daac.jpg"
+  }
+]);
